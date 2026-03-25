78257c1757c45d7ca2ca980dc5dcec423e110f83
Benedictnelson777 <benmwenda375@gmail.com>
2026-03-25 20:16:36 +0300

    Change greeting from 'Hello World' to 'Goodbye World'

--- app.js
+++ app.js
@@ -1960,7 +1960,23 @@ async function handleSendAIMessage() {
     }
 }
 
-
+// Force unregister of Service Workers and clear caches (Cache Busting)
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+            registration.unregister().then(function () {
+                console.log('Force un-registered existing service worker.');
+            });
+        }
+    });
+}
+if ('caches' in window) {
+    caches.keys().then(function (names) {
+        for (let name of names) {
+            caches.delete(name);
+        }
+    });
+}
 
 // Start App
 init();
